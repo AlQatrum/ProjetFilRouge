b239e80dc2d0276fc47d7d5469615aadbf2bfc52
hcordier <71728588+hcordier@users.noreply.github.com>
2021-02-11 17:55:53 +0100

    Update categories_ratio_genre.py

--- Programs/ratio/categories_ratio_genre.py
+++ Programs/ratio/categories_ratio_genre.py
@@ -4,9 +4,10 @@ Created on Sat Dec 26 18:18:19 2020
 
 @author: hippo
 """
-
+"""création des catégories de ratios"""
 import pandas as pd
 
+#importation des données
 df = pd.read_csv('C:/Users/hippo/Desktop/iodaa/Fil_Rouge/ProjetFilRouge/Data/InitialData/ratio_genre_trié_avec_age_transpose.csv',sep=',', header=0, index_col=0 )
 df1 = df.astype({"age": str})
 
@@ -14,6 +15,7 @@ nchild=0
 nadult=0
 nelder=0
 df1index=df1.index
+#on remplie chaque classe avec 59 individus
 for k in range(len(df["age"])):
     if df["age"][k]<7 and nchild<59:
         df1["age"][k]="child"
@@ -32,6 +34,7 @@ df4 = df1[df1.age == "elderly"]
 frames =[df2, df3 , df4]
 result= pd.concat(frames)
 
+#on enregistre le résultat
 result.to_csv("C:/Users/hippo/Desktop/iodaa/Fil_Rouge/ProjetFilRouge/Data/InitialData/ratio_genre_classé_1.csv")
 
 
